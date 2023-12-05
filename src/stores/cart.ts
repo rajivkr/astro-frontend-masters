5f506d25c89dd2418cea014071fd617a6ec82f9f
Rajiv Ramakrishnan <rajiv270992@gmail.com>
2023-12-05 23:22:07 +0530

    feat: adding removeItemFromCart

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -3,27 +3,41 @@ import { computed, map } from 'nanostores';
 export const $cart = map<Record<number, CartItem | undefined>>({});
 
 export function addItemToCart(item: ShopItem) {
-	const cartItem = $cart.get()[item.id];
-	const quantity = cartItem?.quantity || 0;
+  const cartItem = $cart.get()[item.id];
+  const quantity = cartItem?.quantity || 0;
 
-	$cart.setKey(item.id, {
-		item,
-		quantity: quantity + 1,
-	});
+  $cart.setKey(item.id, {
+    item,
+    quantity: quantity + 1,
+  });
+}
+
+export function deleteItemFromCart(item: ShopItem) {
+  $cart.setKey(item.id, undefined);
 }
 
 export function removeItemFromCart(item: ShopItem) {
-	$cart.setKey(item.id, undefined);
+  const cartItem = $cart.get()[item.id];
+  if (cartItem) {
+    if (cartItem.quantity === 1) {
+      deleteItemFromCart(item);
+    } else {
+      $cart.setKey(item.id, {
+        item,
+        quantity: cartItem.quantity - 1,
+      });
+    }
+  }
 }
 
 export const subTotal = computed($cart, (entries) => {
-	let subTotal = 0;
-	Object.values(entries).forEach((entry) => {
-		if (!entry) {
-			return;
-		}
-		subTotal += entry.quantity * entry.item.price;
-	});
+  let subTotal = 0;
+  Object.values(entries).forEach((entry) => {
+    if (!entry) {
+      return;
+    }
+    subTotal += entry.quantity * entry.item.price;
+  });
 
-	return subTotal;
+  return subTotal;
 });
